d251da1d18c4ea799512bbb41ddba4cf31054f4a
bright adams <btadamz@gmail.com>
2024-09-17 15:02:32 +0100

    mongo

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,16 +4,16 @@ import { UserModule } from './user/user.module';
 import { DatabaseModule } from './database/database.module';
 import { TodolistModule } from './todolist/todolist.module';
 import { ConfigModule } from '@nestjs/config';
-
+ 
 @Module({
-  imports: [
+  imports: [  
     UserModule,
     TodolistModule,
     DatabaseModule,
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-  ],
+  ], 
   controllers: [],
   providers: [],
 })
